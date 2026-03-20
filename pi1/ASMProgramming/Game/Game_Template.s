6b7f7095a0cec190353368bca52ee5a6ab21db3c
FilipAnger <filip.anger@gmail.com>
2026-03-20 13:35:46 +0100

    sys call time of day

--- pi1/ASMProgramming/Game/Game_Template.s
+++ pi1/ASMProgramming/Game/Game_Template.s
@@ -170,10 +170,10 @@ gen_number:
         
                                     @ TASK: Load address of time struct to R0
         LDR R1, #0                            @ TASK: Load 0 into R1 (time zone)
-        LDR R7,                            @ TASK: Place system call number for gettimeofday in R7
-        SWI 0  ???                            @ TASK: Make the system call
+        LDR R7, #0xE4                           @ TASK: Place system call number for gettimeofday in R7
+        SWI 0                           @ TASK: Make the system call
         LDR R2, []                           @ TASK: Load a register with address of musecs variable
-        LDR R0, =musecs    ???         @ TASK: Load R0 with the value at address of musecs
+        LDR R0, =musecs            @ TASK: Load R0 with the value at address of musecs
         AND R0, #0x7F                @ TASK: Perform logical AND of R0 with bitmask 0111 1111 
                                     
         MOV     R0, #30             @ Return a fixed value until executing on an RPI 
